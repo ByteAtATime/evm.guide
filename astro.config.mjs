c960c31bb1c1b46a6cb18fe7ab74104313691fab
ByteAtATime <ByteAtATime@proton.me>
2024-04-19 08:31:40 -0700

    Initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,38 @@
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
+
+import tailwind from "@astrojs/tailwind";
+import remarkMath from "remark-math";
+import rehypeKatex from "rehype-katex";
+import starlightImageZoomPlugin from "starlight-image-zoom";
+
+// https://astro.build/config
+export default defineConfig({
+  markdown: {
+    remarkPlugins: [remarkMath],
+    rehypePlugins: [rehypeKatex],
+  },
+  integrations: [
+    starlight({
+      title: "EVM.Guide",
+      plugins: [starlightImageZoomPlugin()],
+      social: {
+        github: "https://github.com/ByteAtATime/evm.guide",
+      },
+      sidebar: [
+        {
+          label: "Book",
+          autogenerate: { directory: "book" },
+        },
+        {
+          label: "Fundamental Concepts",
+          autogenerate: {
+            directory: "concepts",
+          },
+        },
+      ],
+      customCss: ["./src/katex.css", "./src/app.css"],
+    }),
+    tailwind({ applyBaseStyles: false }),
+  ],
+});
